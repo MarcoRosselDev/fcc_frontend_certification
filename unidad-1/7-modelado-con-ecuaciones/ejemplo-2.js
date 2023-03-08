6ba78527d1d44469bdf74f92bb368afc43c24f50
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-08 09:35:45 -0300

    unid 1.7 ejemplo 2 poroblemas sobre interes descanso largo

--- unidad-1/7-modelado-con-ecuaciones/ejemplo-2.js
+++ unidad-1/7-modelado-con-ecuaciones/ejemplo-2.js
@@ -7,3 +7,20 @@ uno de los certificados paga 6% y el otro 4.5% de interes simple al agno.
 
 ---> cuanto dinero se ivierte a cada una de las tasas de interes ?
 */
+
+const interesX = 0.06
+const interesY = 0.045
+const herencia = 100000
+const gananciaTotal = 5025
+
+function invercionesHerencia() {
+    
+    const totalInvercion = gananciaTotal/(interesX - interesY);
+    const gananciaY = interesY * herencia;
+
+    invercionY = gananciaY/totalInvercion;
+    console.log(invercionY);
+
+}
+
+invercionesHerencia()
