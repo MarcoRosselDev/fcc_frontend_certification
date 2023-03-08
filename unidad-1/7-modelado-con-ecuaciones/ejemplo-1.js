a2e791056b39b079fa46bcb352750e47aad03447
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-08 05:58:23 -0300

    modelado con ecuaciones

---
+++ unidad-1/7-modelado-con-ecuaciones/ejemplo-1.js
@@ -0,0 +1,5 @@
+/* una compagnia de renta de autos cobra 30 dolares al dia y 15 centavos por milla para rentar un auto.
+    Helen renta un auto durante dos dias  su cuenta llega a 108 dolares. 
+    -- cuantas millas recorrio ?
+    
+*/
