691bc5a85df23537a3832b22077ab922fd3f7023
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-08 06:26:28 -0300

    modelado con ecuaciones | ejemplo 1

--- unidad-1/7-modelado-con-ecuaciones/ejemplo-1.js
+++ unidad-1/7-modelado-con-ecuaciones/ejemplo-1.js
@@ -1,5 +1,18 @@
 /* una compagnia de renta de autos cobra 30 dolares al dia y 15 centavos por milla para rentar un auto.
     Helen renta un auto durante dos dias  su cuenta llega a 108 dolares. 
     -- cuantas millas recorrio ?
-    
+
 */
+
+/* constantes de valores de la compagnia */
+
+const dia = 30
+const milla = 0.15
+
+/* funcion para calcular solo con una incognita */
+
+const calcularVariablesCompagnia = (dias, millas, totalDolares) {
+    if(dias && millas) {
+        return console.log('wheit a minute');
+    }
+}
