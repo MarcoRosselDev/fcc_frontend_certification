ee98f0706659403051ef61a1b0c0233d84012662
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-09 07:22:39 -0300

    dimension jardin

---
+++ unidad-1/7-modelado-con-ecuaciones/dimension.js
@@ -0,0 +1,15 @@
+/* un jardin cuadrado tiene un andador de 3 pies de ancho alrededor de su borde exterior.
+si el area de todo el jardin, incluyendo los andadores, es de 18 000 pies cuadrados.
+
+    ---> cuales son las dimensiones del area plantada ?
+
+*/
+
+function areaJardin(areaTotal, anchoAndador) {
+
+    const jardin =  Math.round(Math.sqrt(areaTotal) - anchoAndador*2)
+
+    console.log(jardin);
+}
+
+areaJardin(18000, 3) // 128
