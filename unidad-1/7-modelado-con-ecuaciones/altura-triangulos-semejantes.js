008f60eaf331126837922573b6023272f677450c
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-10 15:43:04 -0300

    altura triangulos semejantes

---
+++ unidad-1/7-modelado-con-ecuaciones/altura-triangulos-semejantes.js
@@ -0,0 +1,18 @@
+// Determinar la altura de un edificio usando triangulos semejantes
+
+/*  Un hombre mide 6 pies de estatura desea determinar la altura de cierto edificio de cuatro pisos.
+    Mide la sombra de la construicccion y encuentra que es de 28 pies de largo,
+    mientras que su propia sombra es de 3.5 pies de largo.
+
+    --> cual es la altura del edificio.
+    por tanto sea que       h = la altura del edificio
+
+    h               ==>  altura desconosida edificio 
+    28 pies         ==>  sombra edificio 
+
+    6 pies          ==> altura hombre
+    3.5 pies        ==> sombra hombre
+
+    h/28 = 6/3.5
+
+*/
