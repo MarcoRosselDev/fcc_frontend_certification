31cc1db533329365c0f4958ce5a0dd69f8e2e2de
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-10 16:51:54 -0300

    trabajo en equipo

---
+++ unidad-1/7-modelado-con-ecuaciones/tiempo.js
@@ -0,0 +1,27 @@
+/* Problemas de tiempo necesario para realizar un trabajo
+
+    debido a una fuerte tormenta anticipada el nivel del agua en un estanque debe descender 1 pie.
+    Abrir el vertedero A hace que el nivel descienda en 4 horas, mientras que abrir el vertedero mas peque;o B hace que el nivel del agua descienda en 6 horas.
+
+    --> cuanto tardara en bajar 1 pie el nivel del agua con ambos vertederos abiertos ?
+
+    A = 1/4
+    B = 1/6
+
+    --- si los dos trabajan juntos = 1/x
+
+*/
+
+function trabajoEquipo(trabajadorA, trabajadorB) {
+    
+    resultado = (trabajadorA*trabajadorB)/(trabajadorB + trabajadorA)
+
+    if (resultado % 1 == 0) {
+        console.log(`el trabajo en conjunto se realiza en ${resultado} horas`);
+    } else {
+        console.log(`el trabajo en conjunto se raliza en ${Math.floor(resultado)} horasss`);
+    }
+
+}
+
+trabajoEquipo(4, 6)
