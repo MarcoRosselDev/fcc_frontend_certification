f6967739cf46456fff175ae7cf0cdbbd190874f2
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-10 16:19:19 -0300

    mezclas

---
+++ unidad-1/7-modelado-con-ecuaciones/mezclas.js
@@ -0,0 +1,10 @@
+/* Porblemas de mezclas
+
+    un fabricante de bebidas gaseosas nuncia su refresco de naranja como "con sabor natural".
+    pero solo contiene un 5% de jugo de naranja.
+    un nuevo reglamento federal establece que para ser llamada natural, una bebida debe contener al menos 10% de jugo de fruta.
+
+    --> cuanto jugo de naranja puro debe gregrar este fabricante a 900 galones de refresco de naranga para apegarse al nuevo relgamento ?
+
+*/
+
