1a210b5af3d43141e961c9ad8f0ba0825b26928d
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-10 18:10:31 -0300

    distancia rapidez y tiempo

---
+++ unidad-1/7-modelado-con-ecuaciones/distancia-rapidez-tiempo.js
@@ -0,0 +1,9 @@
+/* Problemas de distancia, rapidez y tiempo
+
+    Un jet volo de nueva york a los angeles una distancia de 4200 kilometros.
+    la rapidez para el viaje de regreso fue de 100km/hora mas rapido que la rapidez en el vuelo de ida.
+    si el viaje total duro 13 horas.
+
+    ---> cual fue la rapidez del jet de nueva york a los angeles ? 
+
+*/
