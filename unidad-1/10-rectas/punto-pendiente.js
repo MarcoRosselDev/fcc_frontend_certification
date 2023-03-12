28ad051d28985df703a255153d8473e9e7e0ab98
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-12 20:24:49 -0300

    unidad 1.10 rectas | ecuacion recta con punto-pendiente

---
+++ unidad-1/10-rectas/punto-pendiente.js
@@ -0,0 +1,11 @@
+/* punto pendiente de la ecuacion de una recta
+
+    la ecuacion de una recta que pasa por el punot (x1, y1) y tiene pendiente m es 
+
+    y - y1 = m(x - x1)
+
+*/
+
+// encuentra la ecuacion de la recta que pasa por (1, -3) pendiente -1/2.
+
+
