91b7e84d251bd8f3e6e0b9de0181ea89f76f6158
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-11 23:01:47 -0300

    inversiones | ejercicio 25 calculo stewart

---
+++ unidad-1/7-modelado-con-ecuaciones/ejercicios1-7/26-inversiones.js
@@ -0,0 +1,7 @@
+/* 26   inversiones
+
+    Si ben invierte 4000 dolares al 4% de interes al a;o
+
+    --> cuanto dinero adicional debe invertir al 5.5% de interes anual para garantizar que el interes que reciba cada a;o sea 4.5% de la cantidad total invertida ?
+
+*/
