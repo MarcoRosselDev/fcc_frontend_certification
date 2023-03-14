5bc9d024551fc62f15ca4941ae9326be4f4c03bf
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-14 20:52:49 -0300

    unidad 4 funciones exponenciales y logaritmicas | 6 modelado funciones exponenciales 2

--- unidad-4/modelado-fun-exponenciales/poblacion-bacterias.js
+++ unidad-4/modelado-fun-exponenciales/poblacion-bacterias.js
@@ -5,8 +5,17 @@ Inicialmente hay 1 000 en una colonia.
 
 a-  Encuentre un modelo para la poblacion de bacterias despues de t horas
 b-  cuantas bacterias hay en la colonia despues de 15 horas ?
-c-  cuando llegara a 100 000 el numero de b acterias ?
+c-  cuando llegara a 100 000 el numero de bacterias ?
 
 */
 
-// 
+//a=         n(t) = 1000 * 2^(t/3)
+
+function poblacionBacterias(tiempo) {
+    const solution = 1000*Math.pow(2, (tiempo/3))
+
+    console.log(`la poblacion es de ${solution} en ${tiempo} horas`);
+}
+
+poblacionBacterias(15)
+//la poblacion es de 32000 en 15 horas
