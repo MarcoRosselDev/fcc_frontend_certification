d5b3205c83be403a1425839e88657ffb63dbccbf
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-14 20:47:59 -0300

    unidad 4 funciones exponenciales y logaritmicas | 6 modelado funciones exponenciales 1

---
+++ unidad-4/modelado-fun-exponenciales/poblacion-bacterias.js
@@ -0,0 +1,12 @@
+/* Poblacion de bacterias
+
+bajo condiciones ideales cierta poblacion de bacterias se duplica cada tres horas. 
+Inicialmente hay 1 000 en una colonia.
+
+a-  Encuentre un modelo para la poblacion de bacterias despues de t horas
+b-  cuantas bacterias hay en la colonia despues de 15 horas ?
+c-  cuando llegara a 100 000 el numero de b acterias ?
+
+*/
+
+// 
