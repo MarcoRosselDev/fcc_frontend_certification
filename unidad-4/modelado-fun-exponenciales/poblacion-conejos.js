307f94bc13f7214277cf895e4f4ad410309155d1
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-14 21:03:03 -0300

    unidad 4 funciones exponenciales y logaritmicas | 6 modelado funciones exponenciales | poblacion conejos

---
+++ unidad-4/modelado-fun-exponenciales/poblacion-conejos.js
@@ -0,0 +1,10 @@
+/* Poblacion conejos
+
+Cierta clase de conejos fue introducida en una peque;a isla hace 8 meses .
+La poblacion actual de conejos en la isla se estima en 4100 y se duplica cada 3 meses.
+
+a   cual fue el tama;o inicial de la poblacion de conejos?
+b   estime la poblacion a un a;o despues de que los conejos fueron introducidos en la isla
+
+*/
+
