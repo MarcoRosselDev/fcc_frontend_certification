97d6daec5015e0296457eb741e935e2932b0058f
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-23 04:06:03 -0300

    angulos coterminales

---
+++ unidad-6/medida-de-algulo/2-angulos-coterminales.js
@@ -0,0 +1,9 @@
+/* dos angulso en posicion estandar son conterminales si sus lado scoinciden.
+
+angulos:
+    sumamos cualquier multiplo de 360.
+
+radianes:
+    sumamos cualquier multiplo de 2pi.
+
+*/
