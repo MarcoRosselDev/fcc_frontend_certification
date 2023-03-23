4fde77a1c4b8cfd24b7c6ee841a77738f5fa10a0
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-23 04:14:43 -0300

    coterminal de angulos function

--- unidad-6/medida-de-algulo/2-angulos-coterminales.js
+++ unidad-6/medida-de-algulo/2-angulos-coterminales.js
@@ -7,3 +7,13 @@ radianes:
     sumamos cualquier multiplo de 2pi.
 
 */
+
+function coterminalAngulo(angulo, multiplo) {
+    respuesta = angulo + 360*multiplo;
+    console.log(respuesta);
+}
+
+coterminalAngulo(30, 2) // 750
+coterminalAngulo(30, 3) // 1110
+coterminalAngulo(30, -2) // -690
+coterminalAngulo(30, -3) // -1050
