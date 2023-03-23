6ad862a9e8aba9f4a774d2dd8891caea1af862ff
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-23 01:25:18 -0300

    ejerc grados a radianes 5 al 16 comprobacion resultados

--- unidad-6/medida-de-algulo/5.js
+++ unidad-6/medida-de-algulo/5.js
@@ -21,30 +21,16 @@ function gradosRadian(grados) {
     console.log(result);
 }
 
-gradosRadian(15)
-gradosRadian(36)
-gradosRadian(54)
-gradosRadian(75)
-gradosRadian(-45)
-gradosRadian(-30)
-gradosRadian(100)
-gradosRadian(200)
-gradosRadian(1000)
-gradosRadian(3600)
-gradosRadian(-70)
-gradosRadian(-150)
-
-/* 
-0.2617993877991494
-0.6283185307179586
-0.9424777960769379
-1.3089969389957472
--0.7853981633974483
--0.5235987755982988
-1.7453292519943295
-3.490658503988659
-17.453292519943297
-62.83185307179586
--1.2217304763960306
--2.6179938779914944
-*/
+//              resultado               n ejericico
+gradosRadian(15) // 0.2617993877991494  5
+gradosRadian(36) // 0.6283185307179586  6
+gradosRadian(54) // 0.9424777960769379  7
+gradosRadian(75) // 1.3089969389957472  8
+gradosRadian(-45) // -0.7853981633974483    9
+gradosRadian(-30) // -0.5235987755982988    10
+gradosRadian(100) // 1.7453292519943295     11
+gradosRadian(200) // 3.490658503988659      12
+gradosRadian(1000) // 17.453292519943297    13
+gradosRadian(3600) // 62.83185307179586     14
+gradosRadian(-70) // -1.2217304763960306    15
+gradosRadian(-150) // -2.6179938779914944   16
