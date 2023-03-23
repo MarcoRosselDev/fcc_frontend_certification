93f3cdd69d8b582e056ea4b52d89aab1d381dc35
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-23 10:15:07 -0300

    coterminal modulo | ejercicio 42 - 46 | corr errores

--- unidad-6/medida-de-algulo/ejercicio-42-46.js
+++ unidad-6/medida-de-algulo/ejercicio-42-46.js
@@ -1,6 +1,10 @@
 function moduloCoterminal(angulo) {
     resultado = angulo % 360;
-    console.log(resultado);
+    if (resultado < 0) {
+        console.log(resultado + 360);
+    } else {
+        console.log(resultado);
+    }
 }
 
 // 42       375 grados
@@ -8,8 +12,8 @@ moduloCoterminal(375) // 15 grados
 // 43       780 grados
 moduloCoterminal(780) // 60 grados
 // 44      -100 grados
-moduloCoterminal(-100) // -100 grados
+moduloCoterminal(-100) // 260 grados
 // 45      -800 grados
-moduloCoterminal(-800) // -80 grados
+moduloCoterminal(-800) // 280 grados
 // 46       1270 grados
 moduloCoterminal(1270) // 190 grados 
