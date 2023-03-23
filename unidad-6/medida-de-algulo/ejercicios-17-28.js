757709b637dfb53dd6ed0a10364dfc9266f4a4ab
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-23 02:08:41 -0300

    ejerc radianes a grados 17 - 28 answers successful

--- unidad-6/medida-de-algulo/ejercicios-17-28.js
+++ unidad-6/medida-de-algulo/ejercicios-17-28.js
@@ -7,41 +7,25 @@ function radianGrados(radian) {
 
 // 17-      ( 5*pi ) / 3
 radianGrados((Math.PI * 5) / 3) // 300
-
 // 18-      3pi / 4
-radianGrados((Math.PI * 3) / 4)
+radianGrados((Math.PI * 3) / 4) // 135
 // 19-      5pi / 6
-radianGrados((Math.PI * 5) / 6)
+radianGrados((Math.PI * 5) / 6) // 150 
 // 20-      -3pi / 2
-radianGrados((Math.PI * -3) / 2)
+radianGrados((Math.PI * -3) / 2) // -270 
 // 21-      3
-radianGrados(3)
+radianGrados(3) // 172 
 // 22-      -2
-radianGrados(-2)
+radianGrados(-2) // -115 
 // 23-      -1.2
-radianGrados(-1.2)
+radianGrados(-1.2) // -69 
 // 24-      3.4
-radianGrados(3.4)
+radianGrados(3.4) // 195 
 // 25-      pi / 10
-radianGrados((Math.PI) / 10)
+radianGrados((Math.PI) / 10) // 18
 // 26-      5pi / 18
-radianGrados((Math.PI * 5) / 18)
+radianGrados((Math.PI * 5) / 18) // 50 
 // 27-      -2pi / 15
-radianGrados((Math.PI * -2) / 15)
+radianGrados((Math.PI * -2) / 15) // -24 
 // 28-      -13pi / 12
-radianGrados((Math.PI * -13) / 12)
-
-/* 
-300
-135
-150
--270
-172
--115
--69
-195
-18
-50
--24
--195
-*/
+radianGrados((Math.PI * -13) / 12) // -195
