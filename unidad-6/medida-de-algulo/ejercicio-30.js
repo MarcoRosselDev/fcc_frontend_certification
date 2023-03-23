d02e68dd67e50d661e7f8f9c3f0d0f8fe475c2cd
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-23 04:49:53 -0300

    coterminal exercice 30

---
+++ unidad-6/medida-de-algulo/ejercicio-30.js
@@ -0,0 +1,11 @@
+function coterminalAngulo(angulo, multiplo) {
+    respuesta = angulo + 360*multiplo;
+    console.log(respuesta);
+}
+
+// 30   135 grados
+
+coterminalAngulo(135, 1) // 495
+coterminalAngulo(135, 2) // 855
+coterminalAngulo(135, -1) // -225
+coterminalAngulo(135, -2) // -58
