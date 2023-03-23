bbb0e7587f80f21d8cfbff1b7c7aac3cb37fe194
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-23 00:27:07 -0300

    radian a grados

---
+++ unidad-6/medida-de-algulo/converitr-raidan-en-grado.js
@@ -0,0 +1,13 @@
+/* 
+
+x radian en grados ====> * pi/180
+x grados en radian ====> * 180/pi
+
+*/
+
+function gradosRadian(grados) {
+    result =  grados * (Math.PI/180)
+    console.log(result);
+}
+
+gradosRadian(60) // 1.0471975511965976
