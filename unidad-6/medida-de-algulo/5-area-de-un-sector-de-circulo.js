7d0c8e1c5716a3441036aefabbe2fac04da44ec5
Marco Rossel <andresmarcorossel@gmail.com>
2023-03-24 01:19:38 -0300

    area de un sector de circulo

---
+++ unidad-6/medida-de-algulo/5-area-de-un-sector-de-circulo.js
@@ -0,0 +1,11 @@
+/* Area de un sector circular
+
+A = (1/2)* radio^2 * angulo
+
+en algulo tiene que estar en radianes, si no no sitve la formula.
+reminder- formula para convertir grados en radianes
+    grados a radianes =    grados* PI/180
+    radianes a grados =    radianes*180/PI
+
+*/
+
