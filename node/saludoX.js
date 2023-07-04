4214a197fb1130843c5c42f144391c0753ff77f5
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-04 14:36:28 -0400

    node course | 01

--- node/saludoX.js
+++ node/saludoX.js
@@ -1,5 +1,11 @@
+const saludo1 = require("./saludosY");
+const saludo2 = require("./saludosY");
+const saludo3 = require("./saludosY");
+const saludo4 = require("./saludosY");
+
 function saludar(name) {
-    return `Buenos dias ${name}`;
+    const a = saludo1.y1('lusho');
+    return `${a} and saludo to ${name}`;
 }
 
 console.log(module.exports);
---
+++ node/saludosY.js
@@ -0,0 +1,17 @@
+function saludo1(name) {
+    return `saludo 1 a ${name}`;
+}
+function saludo2(name) {
+    return `saludo 2 a ${name}`;
+}
+function saludo3(name) {
+    return `saludo 3 a ${name}`;
+}
+function saludo4(name) {
+    return `saludo 4 a ${name}`;
+}
+
+module.exports.y1 = saludo1;
+module.exports.y2 = saludo2;
+module.exports.y3 = saludo3;
+module.exports.y4 = saludo4;
