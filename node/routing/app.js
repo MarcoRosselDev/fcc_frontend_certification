8bbdc4b07394390d2f5de784d43227782bb0abcd
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-07 17:21:32 -0400

    node course | 25 my first routing

--- node/routing/app.js
+++ node/routing/app.js
@@ -1,14 +1,29 @@
 const http = require('http');
-const infoCursos = require('./cursos')
+const infoCursos = require('./cursos');
 
 console.log(infoCursos);
 
 const miPage = http.createServer((req, res)=>{
-    //console.log(req);
-    console.log('Practis node course');
-    //console.log(res);
+    const {method} = req;
+
+    if (method === 'GET') {
+        return manejarSolicitudGET(req, res);
+    } else {
+        console.log(`El metodo no puede ser manejado por el servidor : ${method}`);
+    }
+
     res.end('response');
 });
+
+function manejarSolicitudGET(req, res) {
+    let path = req.url;
+
+    if (path === "/") {
+        res.end(`Method GET and you're in home url`)
+    } else if (path === "/courses") {
+        res.end(JSON.stringify(infoCursos));
+    }
+}
 
 const PORT = 3001;
 
