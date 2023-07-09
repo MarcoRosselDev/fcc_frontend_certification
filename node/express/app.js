aa565fea4f939ee6164e116fb6866abff66a3960
Marco Rossel <andresmarcorossel@gmail.com>
2023-07-09 14:44:58 -0400

    node course | 29 nodemon and express

--- node/express/app.js
+++ node/express/app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const {infoCursos} = require('./cursos.js')
 
 app.get('/', (req, res)=>{
     res.send('my first server with Express !')
 });
 
+app.get('/api/cursos', (req, res)=>{
+    res.send(infoCursos)
+});
+
+app.get('/api/cursos/programacion', (req, res) =>{
+    res.send(infoCursos.programacion)
+})
+
+app.get('/api/cursos/matematicas', (req, res) =>{
+    res.send(infoCursos.matematicas)
+})
+
 const PUERTO = process.env.PORT || 3001;
 
 app.listen(PUERTO, () => {
     console.log(`This server's listening on port ${PUERTO} ... `);
-})
+}) 
